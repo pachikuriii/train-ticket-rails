dd23662837c747ef0a7fdeb93c535d53da8e0ada
pachikuriii <stakahashi0728@gmail.com>
2023-02-21 15:45:05 +0900

    Ex1. 運賃の計算（下り）の実装

--- app/models/gate.rb
+++ app/models/gate.rb
@@ -1,13 +1,17 @@
+# frozen_string_literal: true
+
 # NOTE: Gate = 改札機のイメージ
 class Gate < ApplicationRecord
   FARES = [150, 190].freeze
-
+  MAX_DISTANCE = { FARES[0] => 1, FARES[1] => 2 }.freeze
   validates :name, presence: true, uniqueness: true
   validates :station_number, presence: true, uniqueness: true
 
   scope :order_by_station_number, -> { order(:station_number) }
 
   def exit?(ticket)
-    true
+    entered_station_number = Gate.find(ticket.entered_gate_id).station_number
+    moved_distance = (station_number - entered_station_number).abs
+    MAX_DISTANCE[ticket.fare] >= moved_distance
   end
 end
