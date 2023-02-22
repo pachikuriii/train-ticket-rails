63ba6101d7847d9b296ecf3a0a58b9497065f0eb
pachikuriii <stakahashi0728@gmail.com>
2023-02-22 23:47:07 +0900

    最大距離をハッシュを使わずに書く形へ変更

--- app/models/gate.rb
+++ app/models/gate.rb
@@ -3,15 +3,14 @@
 # NOTE: Gate = 改札機のイメージ
 class Gate < ApplicationRecord
   FARES = [150, 190].freeze
-  MAX_DISTANCE = { FARES[0] => 1, FARES[1] => 2 }.freeze
   validates :name, presence: true, uniqueness: true
   validates :station_number, presence: true, uniqueness: true
 
   scope :order_by_station_number, -> { order(:station_number) }
 
   def exit?(ticket)
-    entered_station_number = ticket.entered_gate.station_number
-    moved_distance = (station_number - entered_station_number).abs
-    !moved_distance.zero? && MAX_DISTANCE[ticket.fare] >= moved_distance
+    moved_distance = (station_number - ticket.entered_gate.station_number).abs
+    max_distance = FARES.index(ticket.fare) + 1
+    !moved_distance.zero? && max_distance >= moved_distance
   end
 end
